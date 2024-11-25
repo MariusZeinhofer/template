33d9b65aca2e2ae4d3f0bbd4defac56e5a8dc3c0
Marius Zeinhofer <marius.zeinhofer@gmail.com>
2024-11-25 16:15:23 +0100

    updated the pyproject.toml, added some sample files to showcase editable installs

---
+++ examples/example.py
@@ -0,0 +1,5 @@
+"""Uses functionality from the template library."""
+
+from template.sample import square_root
+
+print(square_root(4))
---
+++ template/sample.py
@@ -0,0 +1,8 @@
+"""Computes the square root of a number."""
+
+from math import sqrt
+
+
+def square_root(x: float) -> float:
+    """Computes the square root of x."""
+    return sqrt(x)
